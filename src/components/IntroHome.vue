767db4779919fb7edd5e4f2b7fb393806bfbf259
clf94-dev <clf94@hotmail.es>
2021-01-26 09:16:55 +0100

    added struct IntroHome components

--- src/components/IntroHome.vue
+++ src/components/IntroHome.vue
@@ -1,12 +1,56 @@
 <template>
-    <div>
-
-    </div>
+   <b-container fluid>
+       <div v-for="data of introdata" v-bind:key="data.title">
+           <IntroHomeCard :info="data"></IntroHomeCard>
+       </div>
+   </b-container>
 </template>
 
 <script>
+import IntroHomeCard from './IntroHomeCard.vue'
     export default {
-        name: 'IntroHome'
+        name: 'IntroHome',
+        components: {
+            IntroHomeCard
+        },
+        data(){
+            return{
+                introdata: [{
+                    title: "Create and share your photo Stories",
+                    text:"Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
+                    btn:"Get an Invite",
+                    classes: "intro-row reversed dark",
+                    btnStyle:"",
+                    arrow: 0,
+                    src: require("../assets/images/home/desktop/create-and-share.jpg"),
+                    imgAlt:"",
+                    link: false
+                },
+                {
+                    title: "Beautiful stories every time",
+                    text:"We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.",
+                    btn:"View the stories",
+                    classes: "intro-row",
+                    btnStyle:"",
+                    arrow: 1,
+                    src: require("../assets/images/home/desktop/beautiful-stories.jpg"),
+                    imgAlt:"",
+                    link: true
+                },
+                {
+                    title: "Designed for everyone",
+                    text:"Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.",
+                    btn:"View the stories",
+                    classes: "intro-row reversed",
+                    btnStyle:"",
+                    arrow: 1,
+                    src: require("../assets/images/home/desktop/designed-for-everyone.jpg"),
+                    imgAlt:"",
+                    link: true
+                },
+                ]
+            }
+        }
     }
 </script>
 
--- src/components/IntroHomeCard.vue
+++ src/components/IntroHomeCard.vue
@@ -1,12 +1,40 @@
 <template>
-    <div>
+    <b-container fluid>
+<b-row :class="info.classes">
+    <b-col class="img-col" xs="12" sm="6" md="6" lg="6">
+<img :src="info.src" :alt="info.imgAlt">
+    </b-col>
+        <b-col class="text-col" xs="12" sm="6" md="6" lg="6">
+ <h2>{{info.title}}</h2>
+      <p>{{info.text}}</p>
+      <button :class="info.index===0? 'btn white': 'btn'" @click="info.link && goToStories()">
+        <b-row>
+         <b-col xs="8" sm="8" md="8" lg="8">
+                   {{info.btn}}
+         </b-col >
+          <b-col xs="4" sm="4" md="4" lg="4">
+          <img :src="`${info.arrow==0? ArrowW : Arrow }`" alt="arrow">
+          </b-col >
 
-    </div>
+        </b-row>
+
+      </button>
+        </b-col>
+</b-row>
+
+    </b-container>
 </template>
 
 <script>
     export default {
-        name: 'IntroHomeCard'
+        name: 'IntroHomeCard',
+        props: ["info"],
+        data(){
+            return{
+                Arrow: require("../assets/images/shared/desktop/arrow.svg"),
+                ArrowW: require("../assets/images/shared/desktop/arrow copy.svg"),
+            }
+        }
     }
 </script>
 
