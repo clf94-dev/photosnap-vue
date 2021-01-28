ed33b5938f85ee0bc6b5ad57cbcf0a8eabb691ba
clf94-dev <clf94@hotmail.es>
2021-01-28 08:41:34 +0100

    added struct TopFeatures

--- src/components/TopFeatures.vue
+++ src/components/TopFeatures.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
+    <b-container fluid>
+        <b-row class="topfeat-cont">
+            <b-col class="text-col" xs="12" sm="7" md="5" lg="5">
+                  <h2>Features</h2>
+        <p>We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories.</p>
 
-    </div>
+            </b-col>
+            <b-col class="img-col" xs="12" sm="5" md="7" lg="7">
+                <img :src="topFeatD" :srcset="`${topFeatD} 1000w, ${topFeatT} 800w, ${topFeatM} 600w`" alt="Features">
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
     export default {
-        name: 'TopFeatures'
+        name: 'TopFeatures',
+        data(){
+            return{
+                topFeatD: require("../assets/images/features/desktop/hero.jpg"),
+                topFeatT: require("../assets/images/features/tablet/hero.jpg"),
+                topFeatM: require("../assets/images/features/mobile/hero.jpg"),
+            }
+        }
     }
 </script>
 
