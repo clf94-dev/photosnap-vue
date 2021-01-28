a9618ab501d54ded2e056a133e9f876ca18b9ec1
clf94-dev <clf94@hotmail.es>
2021-01-28 08:10:39 +0100

    added struct FeaturesHome comp

--- src/components/FeaturesHome.vue
+++ src/components/FeaturesHome.vue
@@ -1,13 +1,44 @@
 <template>
 
-    <div>
-
-    </div>
+    <b-container fluid>
+        <b-row class="feat-cont">
+            <b-col class="feat-card" xs="12"  sm="4" md="4" lg="4" v-for="data of featuresdata" v-bind:key="data.title">
+<img :src="data.src" :alt="data.imgAlt"/>
+        <h3>{{data.title}}</h3>
+        <p>{{data.text}}</p>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
     export default {
-        name: 'FeaturesHome'
+        name: 'FeaturesHome',
+        data(){
+            return{
+                featuresdata:[
+                    {
+                        title: "100% Responsive",
+                        text:"No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen.",
+                        src: require("../assets/images/features/desktop/responsive.svg"),
+                        imgAlt:"100% Responsive"
+                    },
+                    {
+                        title: "No Photo Upload Limit",
+                        text:"Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go.",
+                        src: require("../assets/images/features/desktop/no-limit.svg"),
+                        imgAlt:"No Photo Upload Limit"
+                    },
+                    {
+                        title: "Available to Embed",
+                        text:"Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more.",
+                        src: require("../assets/images/features/desktop/embed.svg"),
+                        imgAlt:"Available to Embed"
+                    },
+
+                ]
+            }
+        }
     }
 </script>
 
