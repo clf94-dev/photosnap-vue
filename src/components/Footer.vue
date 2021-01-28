78718762012afad2e3796c9235d843058ecdbc0f
clf94-dev <clf94@hotmail.es>
2021-01-28 08:33:53 +0100

    added style footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
            <b-col class="logo-col col" xs="12" sm="12" md="3" lg="3">
                 <img :src="logo" alt="Photosnap" />
         <b-row class="social-row" >
-            <i className="faa fa-facebook faceb fa-gradient"></i>
+            <i className="fab fa-facebook faceb fa-gradient"></i>
             <i className="fab fa-youtube yout"></i>
             <i className="fab fa-twitter twitt"></i>
             <i className="fab fa-pinterest pint"></i>
@@ -49,5 +49,120 @@
 </script>
 
 <style scoped>
+.footer-section {
+  padding: 7%;
+  background-color: black;
+  color: white;
+}
+
+@media screen and (max-width: 756px) {
+  .footer-section {
+    text-align: center;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .footer-section .col {
+    width: 100%;
+  }
+}
+
+@media screen and (min-width: 901px) {
+  .footer-section .col {
+    width: 25%;
+  }
+}
+
+@media screen and (max-width: 900px) and (min-width: 601px) {
+  .footer-section .col {
+    width: 50%;
+  }
+}
+
+.footer-section .logo-col .social-row {
+  margin: 20px auto;
+}
+
+@media screen and (min-width: 756px) {
+  .footer-section .logo-col .social-row {
+    margin-top: 25%;
+  }
+}
+
+.footer-section .logo-col .social-row i {
+  margin-right: 10px;
+  cursor: pointer;
+  color: white;
+  font-size: 20px !important;
+}
+
+.footer-section .logo-col .social-row .faceb:hover {
+  background: -webkit-linear-gradient(45deg, #6028F1, #63AFDB);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.footer-section .logo-col .social-row .yout:hover {
+  background: -webkit-linear-gradient(45deg, #D3205A, #FF5A5A);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.footer-section .logo-col .social-row .twitt:hover {
+  background: -webkit-linear-gradient(45deg, #27718A, #65FFEB);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.footer-section .logo-col .social-row .pint:hover {
+  background: -webkit-linear-gradient(45deg, #DE3838, #F6C683);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.footer-section .logo-col .social-row .instag:hover {
+  background: -webkit-linear-gradient(45deg, #5A77FF, #BC7198, #FFC593);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+@media screen and (max-width: 756px) {
+  .footer-section .footer-links {
+    margin-bottom: 45px;
+  }
+}
+
+.footer-section .footer-links .single-link {
+  text-decoration: none !important;
+  line-height: 2.2rem;
+  text-decoration: none !important;
+  color: white;
+  font-size: 18px;
+  margin: 12px auto !important;
+}
+
+.footer-section .copy-col .btn {
+  width: 100%;
+  height: 50px;
+  background-color: transparent;
+  border: none;
+  font-size: 15px;
+  color: #ffffff;
+  text-align: start;
+  text-transform: uppercase;
+}
+
+.footer-section .copy-col .btn.white {
+  color: #ffffff !important;
+}
+
+.footer-section .copy-col .btn:hover {
+  transition: all 0.3s ease;
+  cursor: pointer;
+}
+
+.footer-section .copy-col p {
+  margin-top: 25px;
+}
 
 </style>
